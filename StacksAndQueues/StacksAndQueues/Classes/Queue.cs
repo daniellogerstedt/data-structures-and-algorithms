00e2c4ac5e0e68319b5995f378efee1837c7b539
daniel.logerstedt@gmail.com <daniel.logerstedt@gmail.com>
2019-04-01 10:41:22 -0700

    Finished building queues and testing both stacks and queues

--- StacksAndQueues/StacksAndQueues/Classes/Queue.cs
+++ StacksAndQueues/StacksAndQueues/Classes/Queue.cs
@@ -4,7 +4,52 @@ using System.Text;
 
 namespace StacksAndQueues.Classes
 {
-    class Queue
+    public class Queue<T>
     {
+        public Stack<T> Front { get; set; }
+        public Stack<T> Back { get; set; }
+
+        public Queue ()
+        {
+            Front = new Stack<T>();
+            Back = new Stack<T>();
+        }
+
+        /// <summary>
+        /// Takes in a value and puts it in the back of the Queue as a new Node.
+        /// </summary>
+        /// <param name="val">The value to be placed in the new Node.</param>
+        public void Enqueue(T val)
+        {
+            Back.Push(val);
+        }
+
+        /// <summary>
+        /// Cycles the Queue forward (Back Stack to Front Stack if Front is Empty) and then removes and returns the front Node.
+        /// </summary>
+        /// <returns>The Node at the front of the Queue.</returns>
+        public Node<T> Dequeue()
+        {
+            if (Front.Peek() == null) CycleQueue();
+            return Front.Pop();
+        }
+
+        /// <summary>
+        /// Cycles the Queue if nessisary then returns the Front Node without removing it.
+        /// </summary>
+        /// <returns>The Front Node in the Queue.</returns>
+        public Node<T> Peek()
+        {
+            if (Front.Peek() == null) CycleQueue();
+            return Front.Peek();
+        }
+
+        /// <summary>
+        /// Helper method that moves the entire Back Stack into the Front Stack so that the node at the "Front" of the Queue is now at the Top of the Front Stack.
+        /// </summary>
+        private void CycleQueue()
+        {
+            while (Back.Peek() != null) Front.Push(Back.Pop());
+        }
     }
 }
--- StacksAndQueues/TestStacksAndQueues/UnitTest1.cs
+++ StacksAndQueues/TestStacksAndQueues/UnitTest1.cs
@@ -1,14 +1,110 @@
 using System;
 using Xunit;
+using StacksAndQueues.Classes;
 
 namespace TestStacksAndQueues
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestEmptyStack()
         {
+            Stack<string> stack = new Stack<string>();
+            Assert.Null(stack.Top);
+        }
 
+        [Fact]
+        public void TestEmptyQueue()
+        {
+            Queue<string> queue = new Queue<string>();
+            Assert.Null(queue.Front.Top);
+            Assert.Null(queue.Back.Top);
+        }
+
+        [Fact]
+        public void TestPushValue()
+        {
+            Stack<string> stack = new Stack<string>();
+            Assert.Null(stack.Top);
+            stack.Push("Test");
+            Assert.Equal("Test", stack.Top.Data);
+
+        }
+
+        [Fact]
+        public void TestPushNode()
+        {
+            Stack<string> stack = new Stack<string>();
+            Assert.Null(stack.Top);
+            Node<string> node = new Node<string> { Next = null, Data = "Test" };
+            stack.Push(node);
+            Assert.Equal(node, stack.Top);
+        }
+
+        [Fact]
+        public void TestPop()
+        {
+            Stack<string> stack = new Stack<string>();
+            Node<string> node = new Node<string> { Next = null, Data = "Test" };
+            stack.Push(node);
+            Assert.Equal(node, stack.Pop());
+            Assert.Null(stack.Top);
+        }
+
+        [Fact]
+        public void TestEnqueue()
+        {
+            Queue<string> queue = new Queue<string>();
+            Assert.Null(queue.Front.Top);
+            Assert.Null(queue.Back.Top);
+            queue.Enqueue("Test");
+            Assert.Null(queue.Front.Top);
+            Assert.Equal("Test", queue.Back.Top.Data);
+        }
+
+        [Fact]
+        public void TestDequeue()
+        {
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("Test");
+            Assert.Equal("Test", queue.Dequeue().Data);
+            Assert.Null(queue.Front.Top);
+            Assert.Null(queue.Back.Top);
+        }
+
+        [Fact]
+        public void TestPeekStack()
+        {
+            Stack<string> stack = new Stack<string>();
+            Node<string> node = new Node<string> { Next = null, Data = "Test" };
+            stack.Push(node);
+            Assert.Equal(node, stack.Peek());
+            Assert.NotNull(stack.Top);
+
+        }
+
+        [Fact]
+        public void TestPeekQueue()
+        {
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("Test");
+            Assert.Equal("Test", queue.Peek().Data);
+            Assert.NotNull(queue.Front.Top);
+        }
+
+
+        [Fact]
+        public void TestPeekStackNull()
+        {
+            Stack<string> stack = new Stack<string>();
+            Assert.Null(stack.Peek());
+        }
+
+        [Fact]
+        public void TestPeekQueueNull()
+        {
+            Queue<string> queue = new Queue<string>();
+            Assert.Null(queue.Peek());
         }
     }
 }
