c09270cce89abdb9ee07feb5945c6f9d7cc7c97b
daniel.logerstedt@gmail.com <daniel.logerstedt@gmail.com>
2019-04-03 14:12:16 -0700

    Added summary comments

--- FIFOAnimalShelter/FIFOAnimalShelter/Classes/AnimalShelter.cs
+++ FIFOAnimalShelter/FIFOAnimalShelter/Classes/AnimalShelter.cs
@@ -9,6 +9,10 @@ namespace FIFOAnimalShelter.Classes
         public Queue<DateTime> Cats { get; set; }
         public Queue<DateTime> Dogs { get; set; }
 
+        /// <summary>
+        /// Takes an animal and enqueues it.
+        /// </summary>
+        /// <param name="animal">The animal to enqueue.</param>
         public void Enqueue(Animal animal)
         {
             animal.Data = new DateTime();
@@ -16,7 +20,12 @@ namespace FIFOAnimalShelter.Classes
             else if (animal is Cat) Cats.Enqueue(animal);
         }
 
-        public Node<DateTime> Dequeue(string type)
+        /// <summary>
+        /// Dequeues a specific animal type or the oldest animal.
+        /// </summary>
+        /// <param name="type">the Type of animal</param>
+        /// <returns>The oldest animal of the requested type</returns>
+        public Node<DateTime> Dequeue(string type = null)
         {
             if (type == "dog") return Dogs.Dequeue();
             else if (type == "cat") return Cats.Dequeue();
