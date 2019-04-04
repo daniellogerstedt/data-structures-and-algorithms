7f51d30c1beb8ed8d6f252c65b4699d382714955
daniel.logerstedt@gmail.com <daniel.logerstedt@gmail.com>
2019-04-04 10:18:22 -0700

    scaffolded and built method for bracket validation.

---
+++ MultiBracketValidation/MultiBracketValidation/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using StacksAndQueues.Classes;
+
+namespace MultiBracketValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static bool Validate(string input)
+        {
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                char current = input[i];
+                switch (current)
+                {
+                    case '(':
+                        stack.Push(')');
+                        break;
+                    case '[':
+                        stack.Push(']');
+                        break;
+                    case '{':
+                        stack.Push('}');
+                        break;
+                    case ')':
+                        if (!stack.Peek().Data.Equals(current)) return false;
+                        stack.Pop();
+                        break;
+                    case ']':
+                        if (!stack.Peek().Data.Equals(current)) return false;
+                        stack.Pop();
+                        break;
+                    case '}':
+                        if (!stack.Peek().Data.Equals(current)) return false;
+                        stack.Pop();
+                        break;
+                }
+            }
+            if (stack.Peek() != null) return false;
+            return true;
+        }
+    }
+}
