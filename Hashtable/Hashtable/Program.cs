bc87060587c9303b58eaf1e6e3e10baed7cec0cd
daniel.logerstedt@gmail.com <daniel.logerstedt@gmail.com>
2019-04-30 08:31:35 -0700

    Wrote code implementation and testing for challenge 31, added solution image to ReadMe

--- Hashtable/Hashtable/Program.cs
+++ Hashtable/Hashtable/Program.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using Hashtable.Classes;
+using System;
 
 namespace Hashtable
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+        }
+
+        public static string RepeatedWord(string str)
+        {
+            char[] delimiters = { ' ', ',', '.', '\\', ';', ':', '/', '<', '>', '"', '&', '*', '(', ')', '!', '@', '#', '$', '%', '^', '-', '_', '+', '=', '\'', '[', ']',  '|', '`', '~', '\n', '\t' };
+
+            string[] words = str.Split(delimiters);
+
+            Table table = new Table(1024);
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (table.Contains(words[i])) return words[i];
+                table.Add(words[i], words[i]);
+            }
+
+            return null;
         }
     }
 }
